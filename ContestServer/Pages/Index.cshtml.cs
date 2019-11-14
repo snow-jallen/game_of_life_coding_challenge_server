fe598b36e6d597b999dac7da4425462f63779f28
Jonathan Allen <jallen@snow.edu>
2019-11-14 11:54:08 -0700

    Add basic stats

--- ContestServer/Pages/Index.cshtml.cs
+++ ContestServer/Pages/Index.cshtml.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Contest.Shared;
+using ContestServer.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -9,8 +11,18 @@ namespace ContestServer
 {
     public class IndexModel : PageModel
     {
+        private readonly IContestantService contestantService;
+
+        public IndexModel(IContestantService contestantService)
+        {
+            this.contestantService = contestantService ?? throw new ArgumentNullException(nameof(contestantService));
+        }
+
+        public IEnumerable<Contestant> Contestants { get; private set; }
+
         public void OnGet()
         {
+            Contestants = contestantService.GetContestants();
         }
     }
 }
