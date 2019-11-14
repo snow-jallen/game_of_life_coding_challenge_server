398d684818de10ba204f21324332dcf96dfa71c9
Jonathan Allen <jallen@snow.edu>
2019-11-14 11:19:19 -0700

    Add register controller

---
+++ Contest.Shared/RegisterResponse.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Contest.Shared
+{
+    public class RegisterResponse
+    {
+        public Guid Token { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ ContestServer/Controllers/RegisterController.cs
@@ -0,0 +1,30 @@
+﻿using Contest.Shared;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContestServer.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class RegisterController : ControllerBase
+    {
+        public RegisterController()
+        {
+
+        }
+
+        [HttpGet]
+        public RegisterResponse Get([FromBody]RegisterRequest request)
+        {
+            var response = new RegisterResponse();
+            response.Name = request.Name;
+            response.Token = Guid.NewGuid();
+
+            return response;
+        }
+    }
+}
