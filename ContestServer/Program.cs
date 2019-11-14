54e029ca79d7782d42b9aa2d7db1618340be6c16
Jonathan Allen <jallen@snow.edu>
2019-11-14 10:30:31 -0700

    fix port

--- ContestServer/Program.cs
+++ ContestServer/Program.cs
@@ -20,7 +20,19 @@ namespace ContestServer
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
-                    webBuilder.UseStartup<Startup>();
+                    webBuilder.UseStartup<Startup>().UsePort();
                 });
+    }
+
+
+    public static class WebHostBuilderExtensions
+    {
+        public static IWebHostBuilder UsePort(this IWebHostBuilder builder)
+        {
+            var port = Environment.GetEnvironmentVariable("PORT");
+            if (string.IsNullOrEmpty(port))
+                return builder;
+            return builder.UseUrls($"http://+:{port}");
+        }
     }
 }
