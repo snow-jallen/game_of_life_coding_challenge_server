d0b4877a01d0860559c6afab339bd330244a3533
Jonathan Allen <jallen@snow.edu>
2019-11-14 09:21:40 -0700

    Add project files.

---
+++ GrpcClient/Program.cs
@@ -0,0 +1,21 @@
+﻿using Grpc.Net.Client;
+using GrpcServer;
+using System;
+
+namespace GrpcClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Press [enter] when you're ready to connect");
+            Console.ReadLine();
+
+            var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            var client = new Greeter.GreeterClient(channel);
+            var reply = client.SayHello(new HelloRequest { Name = "Jonathan" });
+            Console.WriteLine($"Greeting: {reply.Message}");
+            Console.ReadKey();
+        }
+    }
+}
